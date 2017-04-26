4980a4a650b20611bf2fb24d8056f99ce8f9adaa
ytx <ytx123013@gmail.com>
2017-04-26 11:13:05 +0800

    Initial Commit

---
+++ tiku/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tiku
+//
+//  Created by aaa on 2017/4/26.
+//  Copyright © 2017年 ytx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tiku/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tiku
+//
+//  Created by aaa on 2017/4/26.
+//  Copyright © 2017年 ytx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
